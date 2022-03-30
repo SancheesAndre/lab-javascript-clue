32770c669ca7512a928d4c0fab4cb3f687a24d00
Andre Sanches <andrecsanches2@gmail.com>
2022-03-30 13:38:33 -0300

    Lab done

--- src/clue.js
+++ src/clue.js
@@ -3,60 +3,61 @@
 // Suspects Array
 
 
-const suspectsArray = [{
-  firstName: 'Jacob',
-  lastName: 'Green',
-  occupation: 'Entrepreneur',
-  age: 45,
-  description: 'He has a lot of connections',
-  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-  color: 'green'
-},
-{
-  firstName: 'Doctor',
-  lastName: 'Orchid',
-  occupation: 'Scientist',
-  age: 26,
-  description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-  image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-  color: 'white'
-},
-{
-  firstName: 'Victor',
-  lastName: 'Plum',
-  occupation: 'Designer',
-  age: 22,
-  description: 'Billionaire video game designer',
-  image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-  color: 'purple'
-},
-{
-  firstName: 'Kasandra',
-  lastName: 'Scarlet',
-  occupation: 'Actor',
-  age: 31,
-  description: 'She is an A-list movie star with a dark past',
-  image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-  color: 'red',
-},
-{
-  firstName: 'Eleanor',
-  lastName: 'Peacock',
-  occupation: 'Socialité',
-  age: 36,
-  description: 'She is from a wealthy family and uses her status and money to earn popularity',
-  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-  color: 'blue',
-},
-{
-  firstName: 'Jack',
-  lastName: 'Mustard',
-  occupation: 'Retired Football player',
-  age: 62,
-  description: 'He is a former football player who tries to get by on his former glory',
-  image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-  color: 'yellow',
-}
+const suspectsArray = [
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
+  },
+  {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white'
+  },
+  {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple'
+  },
+  {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red',
+  },
+  {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue',
+  },
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow',
+  }
 ];
 
 // Rooms Array
@@ -123,14 +124,30 @@ const weaponsArray = [
 
 // ITERATION 2
 
-function selectRandom() { }
+function selectRandom(arr) {
+  let randomNumber = Math.floor(Math.random() * arr.length)
+  return arr[randomNumber]
+}
 
-function pickMystery() { }
+function pickMystery() {
+ return {
+   suspect: selectRandom(suspectsArray),
+   weapon:  selectRandom(weaponsArray),
+   room: selectRandom(roomsArray)
+ } 
+ }
 
 
 // ITERATION 3
 
-function revealMystery() { }
+function revealMystery(envelope) {
+  let suspectName = envelope.suspect.firstName
+  let suspectSurname = envelope.suspect.lastName
+  let weaponName = envelope.weapon.name
+  let roomName = envelope.room.name
+
+  return `${suspectName} ${suspectSurname} killed Mr. Boddy using the ${weaponName} in the ${roomName}!`
+ }
 
 
 
